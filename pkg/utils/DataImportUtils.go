a701caf3b0c60fd1329de73a9e3df925be2484be
Vlad <xenonbat@yandex.ru>
2019-07-17 21:43:22 +0300

    Functions in MaprDBUpdater extracted

--- pkg/utils/DataImportUtils.go
+++ pkg/utils/DataImportUtils.go
@@ -5,11 +5,13 @@ import (
 	"bytes"
 	"encoding/csv"
 	"encoding/json"
+	"fmt"
 	"log"
 	"os"
 	"os/exec"
 	"strconv"
 	"strings"
+	"sync"
 	"vmrenter/pkg/models"
 
 	"github.com/fatih/structs"
@@ -170,4 +172,31 @@ func getNodeOS() {
 
 func nodeCSVToJSonFile() {
 
+}
+
+func CreateNodeDBJsons(nodes []models.Node) []map[string]interface{} {
+	listOfMaps := make([]map[string]interface{}, 0) // List of NodeDBJsons
+	var wg sync.WaitGroup
+	nodeDbJsonQueue := make(chan map[string]interface{}, 1)
+	wg.Add(len(nodes))
+
+	fmt.Println("Starting creating NodeDBJsons from nodes...")
+	for _, node := range nodes {
+		go func(node models.Node) {
+			mapIntface := GetNodeJsonDocMap(node)
+			mapIntface["_id"] = node.ID
+			nodeDbJsonQueue <- mapIntface
+		}(node)
+	}
+
+	go func() {
+		for n := range nodeDbJsonQueue {
+			listOfMaps = append(listOfMaps, n)
+			wg.Done()
+		}
+	}()
+
+	wg.Wait()
+	fmt.Println("Finished creating NodeDBJsons from nodes!")
+	return listOfMaps
 }
--- upd/MaprDBUpdater.go
+++ upd/MaprDBUpdater.go
@@ -14,64 +14,8 @@ import (
 
 var nodesTable = "/user/mapr/nodes"
 
-func update(c *cli.Context) error {
-
-	if !c.IsSet("urldbconn") {
-		fmt.Println("Connection string isn't set, aborting")
-		return nil
-	}
-
-	if !c.IsSet("nodesfile"){
-		fmt.Println("File path string isn't set, aborting")
-		return nil
-	}
-	config.SetURLDBConn(c.String("urldbconn"))
-	dbConn := config.GetURLDBConn()
-	var csvFilePath = c.String("nodesfile")
-
-	fmt.Printf("**** Config **** %v\n", dbConn)
-
-	// Getting nodes id, ExpiresAt and ClusterID from /user/mapr/nodes table
-	fmt.Println("Starting getting nodes id, ExpiresAT, ClusterID...")
-	partialNodes := mapr.GetPartialReservationsForNodesUpdate()
-	err := mapr.Reset(nodesTable)
-	if err != nil {
-		fmt.Printf("Error occured while resetting /user/mapr/nodes table: %v", err)
-		return err
-	}
-	fmt.Println("Finished getting nodes id, ExpiresAT, ClusterID!")
-
-	// Getting nodes from csv file
-	fmt.Println("Starting getting nodes from the csv file...")
-	nodes := utils.GetNodesFromCSV(csvFilePath)
-	fmt.Println("Finished getting nodes from the csv file!")
-
-	listOfMaps := make([]map[string]interface{}, 0) // List of NodeDBJsons
-
-	// Creating NodeDBJsons from nodes
-	var wg sync.WaitGroup
-	nodeDbJsonQueue := make(chan map[string]interface{}, 1)
-	wg.Add(len(nodes))
-
-	fmt.Println("Starting creating NodeDBJsons from nodes...")
-	for _, node := range nodes {
-		go func(node models.Node) {
-			mapIntface := utils.GetNodeJsonDocMap(node)
-			mapIntface["_id"] = node.ID
-			nodeDbJsonQueue <- mapIntface
-		}(node)
-	}
-
-	go func() {
-		for n := range nodeDbJsonQueue {
-			listOfMaps = append(listOfMaps, n)
-			wg.Done()
-		}
-	}()
-
-	wg.Wait()
-	fmt.Println("Finished creating NodeDBJsons from nodes!")
-
+// Creating NodeDBJsons from nodes
+func updateNodeDBJsons(partialNodes []models.PartialReservationForNodesUpdate, listOfMaps []map[string]interface{}) {
 	// Updating NodeDBJsons with ClusterID, ExpiresAT
 	wg1 := sync.WaitGroup{}
 	m := &sync.Mutex{}
@@ -93,6 +37,39 @@ func update(c *cli.Context) error {
 	}
 
 	wg1.Wait()
+}
+
+
+func update(c *cli.Context) error {
+
+	if !c.IsSet("urldbconn") {
+		fmt.Println("Connection string isn't set, aborting")
+		return nil
+	}
+
+	if !c.IsSet("nodesfile") {
+		fmt.Println("File path string isn't set, aborting")
+		return nil
+	}
+	config.SetURLDBConn(c.String("urldbconn"))
+	dbConn := config.GetURLDBConn()
+	var csvFilePath = c.String("nodesfile")
+
+	fmt.Printf("**** Config **** %v\n", dbConn)
+
+	partialNodes, err := mapr.ExtractPartialNodesData()
+	if err != nil {
+		panic(err)
+	}
+
+	// Getting nodes from csv file
+	fmt.Println("Starting getting nodes from the csv file...")
+	nodes := utils.GetNodesFromCSV(csvFilePath)
+	fmt.Println("Finished getting nodes from the csv file!")
+
+	listOfMaps := utils.CreateNodeDBJsons(nodes)
+
+	updateNodeDBJsons(partialNodes, listOfMaps)
 
 	// Resetting /user/mapr/node table
 	fmt.Println("Starting resetting nodes table...")
@@ -103,32 +80,7 @@ func update(c *cli.Context) error {
 	}
 	fmt.Println("Finished resetting nodes table!")
 
-	// Updating the nodes table
-	fmt.Println("Starting writing to nodes table...")
-
-	// Synchronous way to update table until the error with goroutines is fixed
-	for _, mapIntface := range listOfMaps {
-		writeErr := mapr.WriteToDBWithTableMap(mapIntface, "/user/mapr/nodes")
-		if writeErr != nil {
-			fmt.Println("Error writing to table", writeErr)
-		}
-	}
-
-	// Asynchronous writing to the table - fails because of syncPut(). Uncomment when the bug is fixed.
-	//var wg2 = sync.WaitGroup{}
-	//for _, mapIntface := range listOfMaps {
-	//	wg2.Add(1)
-	//	go func(mapIntface map[string]interface{}) {
-	//		defer wg2.Done()
-	//		writeErr := mapr.WriteToDBWithTableMap(mapIntface, "/user/mapr/nodes")
-	//		if writeErr != nil {
-	//			fmt.Println("Error writing to table", writeErr)
-	//		}
-	//	}(mapIntface)
-	//}
-	//wg2.Wait()
-	fmt.Println("Finished writing to nodes table!")
-
+	mapr.UpdateNodesTable(listOfMaps)
 	return nil
 
 }
